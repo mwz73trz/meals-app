ef6541d194c40c03bb8b65cc2f5221816fb36e5d
mwz73trz <michaelzura@bellsouth.net>
2022-09-06 05:49:55 -0400

    initial commit

---
+++ meals/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Favorites from "./components/Favorites";
+import Modal from "./components/Modal";
+import Meals from "./components/Meals";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <main>
+      {/* <Search /> */}
+      {/* <Favorites /> */}
+      <Meals />
+      {/* <Modal /> */}
+    </main>
+  );
+}
+
+export default App;
